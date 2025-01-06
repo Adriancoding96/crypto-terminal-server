2948693bb3413d09615be1699f47aa56e1603d8c
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-06 19:21:08 +0000

    **Added** first commit contains general project structure

---
+++ src/main/java/com/adrian/crypto_terminal_server/CryptoTerminalServerApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.crypto_terminal_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTerminalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTerminalServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrian/crypto_terminal_server/CryptoTerminalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrian.crypto_terminal_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTerminalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
