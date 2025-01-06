6bca3930076ddda85df0306b66695365132738d3
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-06 21:19:21 +0000

    **Added** documentation to TransactionMapperTests

--- src/test/java/com/adrian/crypto_terminal_server/TransactionMapperTests.java
+++ src/test/java/com/adrian/crypto_terminal_server/TransactionMapperTests.java
@@ -11,9 +11,15 @@ import com.adrian.crypto_terminal_server.model.Transaction;
 import com.adrian.crypto_terminal_server.record.TransactionRecord;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/*
+ * Tests to verify the correct functionality of TransactionMapper.
+ * The tests validate two methods, which are mapping a Transaction to
+ * a TransactionRecord, and mapping a TransactionRecord to a Transaction.
+ * */
 @SpringBootTest
 public class TransactionMapperTests {
 
@@ -34,6 +40,12 @@ public class TransactionMapperTests {
     mapper = new TransactionMapper();
   }
 
+  /*
+   * Test that ensures TransactionMapper toTransaction method
+   * produces a valid transaction model, that is not null and all attributes
+   * of the record are correctly mapped.
+   * */
+  @DisplayName("Map - TransactionRecord -> Transaction")
   @Test
   void testToTransaction() {
     
@@ -51,11 +63,35 @@ public class TransactionMapperTests {
     assertEquals("produceruuid", transaction.getProducerUuid());
     assertEquals("consumeruuid", transaction.getProducerUuid());
     assertEquals("bitcoin", transaction.getCurrency());
+    assertEquals(2.4D, transaction.getAmount());
     assertEquals(EXAMPLE_DATE_TIME, transaction.getTime());
   }
 
+  /*
+   * Test that ensures TransactionMaper toRecord method
+   * produces a valid TransactionRecord, that is not null and all attributes
+   * are correctly mapped.
+   * */
+  @DisplayName("Map - Transaction -> TransactionRecord")
   @Test
   void testToRecord() {
+    
+    Transaction transaction = new Transaction();
+    transaction.setId(1L);
+    transaction.setProducerUuid("produceruuid");
+    transaction.setConsumerUuid("consumeruuid");
+    transaction.setCurrency("bitcoin");
+    transaction.setAmount(2.4D);
+    transaction.setTime(EXAMPLE_DATE_TIME);
+
+    TransactionRecord record = mapper.toRecord(transaction);
+
+    assertNotNull(record);
+    assertEquals("produceruuid", record.producerUuid());
+    assertEquals("consumeruuid", record.consumerUuid());
+    assertEquals("bitcoin", record.currency());
+    assertEquals(2.4D, record.amount());
+    assertEquals(EXAMPLE_DATE_TIME, record.time());
 
   }
   
