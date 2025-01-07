d0e915d350cefbc3d78000e8256e8469f0155a90
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-01-07 12:18:29 +0000

    **Added** controller to test rabbitmq message queue

---
+++ src/main/java/com/adrian/crypto_terminal_server/controller/RabbitTestController.java
@@ -0,0 +1,27 @@
+package com.adrian.crypto_terminal_server.controller;
+
+import com.adrian.crypto_terminal_server.record.TransactionRecord;
+import com.adrian.crypto_terminal_server.service.TransactionService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class RabbitTestController {
+
+  private final TransactionService transactionService;
+ 
+  @Autowired
+  public RabbitTestController(TransactionService transactionService) {
+    this.transactionService = transactionService;
+  }
+
+  @PostMapping
+  public void testRabbitMQ() {
+    transactionService.sendNewTransaction(new TransactionRecord(null, null, null, null, null));
+  }
+  
+}
